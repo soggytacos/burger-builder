a73d3a384bc7a6f3afe6c774b76b57c15f6631ea
Colman <colman@Transactlys-MacBook-Pro.local>
2020-02-24 15:11:04 -0600

    added the toolbar and navigation functionality for videos up to and through 175

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import burgerLogo from '../../assests/images/burger-logo.png';
+import classes from './Logo.css';
+
+const logo = (props) => (
+    <div className={classes.Logo}>
+        <img src={burgerLogo} alt="MyBurger" />
+    </div>
+);
+
+export default logo;
